800f6b95bcaeefef807f1c34df79cc9332ce2c6c
Pengyu JI <pengyu.ji@etu.utc.fr>
2022-12-18 17:45:34 +0100

    Add report

---
+++ Robotics/NNetwork.R
@@ -0,0 +1,28 @@
+##################
+# Neural Network #
+##################
+if(!require("nnet")) {
+  install.packages("nnet")
+  library("nnet")
+}
+
+set.seed(69)
+K<-20
+folds=sample(1:K,n,replace=TRUE)
+#CV sans scaling
+CV.NN<-rep(0,K)
+for(i in (1:10)){
+  for(local_k in (1:K)){
+    fitNN<-nnet(y ~., data = data[folds!= local_k, ], size = i, linout = TRUE)
+    pred<-predict(fitNN,newdata=data[folds==local_k,])
+    CV.NN[i]<-CV.NN[i]+ sum((data[folds==local_k, 9]- pred)^2)
+  }
+  CV.NN[i]<-CV.NN[i]/n
+}
+plot(1:K, CV.NN, type = 'b', col = 'blue')
+which.min(CV.NN)
+
+fitNN<-nnet(y ~., data = data.train, size = 11, linout = TRUE)
+predNN<-predict(fitNN,newdata=data.test)
+mean((predNN - y.test) ^ 2)#0.01121872
+
---
+++ Robotics/subset.R
@@ -0,0 +1,22 @@
+##########
+# Subset #
+##########
+
+#Packages
+if(!require("leaps")) {
+  install.packages("leaps")
+  library("leaps")
+}
+
+
+#Best Subset---------------------
+fitBestSub <- regsubsets(y~., data = data.train, method = 'exhaustive', nvmax = 8)
+plot(fitBestSub,scale="r2") 
+sumBestSub <- summary(fitBestSub)
+
+rssBestSub<-data.frame(sumBestSub$outmat, RSS=sumBestSub$rss)
+rsquareMaxBestSub <- sumBestSub$outmat[which.max(sumBestSub$adjr2),]#La ligne avec la plus grande adjr2
+rsquareMaxBestSub[rsquareMaxBestSub == '*'] <- as.numeric(1)
+rsquareMaxBestSub[rsquareMaxBestSub == ' '] <- as.numeric(0)
+rsquareMaxBestSub <- as.numeric(rsquareMaxBestSub)#Le masque pour s??lectionner les variables
+regBestSub <- reg.set[c(rsquare_max_forward==1)]
